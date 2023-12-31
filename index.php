b2cca44def9acaa5fc91735e5c264d22280f7026
unknown <sk811f65@gmail.com>
2024-01-01 03:57:58 +0900

    20240101

--- index.php
+++ index.php
@@ -1,4 +1,42 @@
-{
-    "error_code": 9999,
-    "error_message": "Could not login with connection: localhost::10021, username: anonymous"
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>RoutineAchivement</title>
+</head>
+<body>
+
+
+
+<div id="post_page">
+  <form method="post" action="process_post.php">
+    <div>DATE <input type="text" name="DATE" value="<?php echo date("Y-m-d") ?>"></div>
+    <div>TEXT <textarea name="TEXT" rows="10" cols="100"></textarea></div>
+    <div><input type="submit" value="SUBMIT" /></div>
+  </form>
+</div>
+
+<br>
+<br>
+
+<?php
+try {
+    $dbh = new PDO('mysql:host=localhost;dbname=xs603472_routinemanager;charset=utf8', 'xs603472_rm1', 'lemontree' ); 
+    $sql = 'select * from RoutineAchivement2 order by date desc';
+    print('<table border="1"> <tr><th>DATE</th><th>TEXT</th></tr>');
+    foreach ($dbh->query($sql) as $row) {
+        print('<tr><td>'. $row['date']. '</td>');
+        print('<td>'. $row['text']. '</td></tr>');
+    }
+    print('</table>');
+}catch (PDOException $e){
+    print('Error:'.$e->getMessage());
+    die();
 }
+
+$dbh = null;
+?>
+
+
+</body>
+</html>
--- process_post.php
+++ process_post.php
@@ -1,4 +1,32 @@
-{
-    "error_code": 9999,
-    "error_message": "Could not login with connection: localhost::10021, username: anonymous"
-}
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <title>PostProcess</title>
+</head>
+<body>
+
+<?php
+  try   {
+
+    $date = $_POST['DATE'];
+    $text = $_POST['TEXT'];
+    
+    $PDO = new PDO('mysql:host=localhost;dbname=xs603472_routinemanager;charset=utf8', 'xs603472_rm1', 'lemontree' );
+    $PDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+
+    $sql = "INSERT INTO RoutineAchivement2 (date, text) VALUES (:date, :text) ON DUPLICATE KEY UPDATE text = VALUES(text)"; 
+    $stmt = $PDO->prepare($sql);
+    $params = array(':date' => $date, ':text' => $text); 
+    $stmt->execute($params);
+
+    header("Location: index.php");
+
+ }catch (PDOException $e){
+    print('Error:'.$e->getMessage());
+    die();
+ }
+
+?>
+</body>
+</html>
